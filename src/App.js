476d39b01f94e27400f30bedb3e4d2428345b293
tabithatrubey <tabithatrubey@outlook.com>
2023-01-05 12:28:22 -0800

    Fetch changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React, { useState } from 'react' 
+import React, { useEffect, useState } from 'react' 
 import Footer from './components/Footer';
 import Header from './components/Header';
 import CoffeeIndex from './pages/CoffeeIndex';
@@ -13,13 +13,42 @@ import mockCoffees from './mockCoffees'
 import { NavLink } from "react-router-dom"
 
 function App() {
-  const [coffees, setCoffees] = useState(mockCoffees)
+  const [coffees, setCoffees] = useState([])
+  useEffect(() => {
+    readCoffee()
+  }, [])
+
+  const readCoffee = () => {
+    fetch("http://localhost:3000/coffees")
+      .then((response) => response.json())
+      .then((payload) => {
+        setCoffees(payload)
+      })
+      .catch((error) => console.log(error))
+  }
   const createCoffee = (coffee)=>{
-    console.log("Created Coffee:", coffee)
+    fetch("https://localhost:3000/coffees", {
+      body: JSON.stringify(coffee),
+      headers: {
+        "content-Type" : "application/json"
+      },
+      method: "POST" 
+    })
+    .then((response) => response.json())
+    .then(() => readCoffee)
+    .catch((error) => console.log("Coffee create error:", error))
   }
   const updateCoffee = (coffee, id) => {
-    console.log("coffee:", coffee)
-    console.log("id:", id)
+    fetch(`http://localhost:3000/coffees/${id}`, {
+      body: JSON.stringify(coffee),
+      headers: {
+        "content-Type" : "application/json"
+      },
+      method: "PATCH"
+    })
+    .then((response) => response.json())
+    .then((payload) => readCoffee())
+    .catch((errors) => console.log("Coffee update errors:", errors))
   }
   return (
 <>
